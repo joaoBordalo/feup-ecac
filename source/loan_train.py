327a34e49d84d9d0f7b153308fa928cd6a61ee7f
João Pereira <pereiraffjoao1993@gmail.com>
2016-12-21 19:29:24 +0000

    means already verified and are correct

--- source/loan_train.py
+++ source/loan_train.py
@@ -23,18 +23,30 @@ def create_status_dataframe(dataframe):
     
     return df
     
-# loading loan train file
-loans_mod = utils.load_dataframe('train_sets/loan_train_modified.csv')
-# create simple dataset
-basic_v1 = create_basic_dataframe(loans_mod)
-basic_v1.drop(basic_v1.columns[0], axis=1)
-utils.write_dataframe_to_CSV(basic_v1, 'dataset_v1.csv')
-# calculate the mean of transactions
-#basic_v2 = trans_train.get_mean_trans_values('train_sets/trans_train_modified.csv', basic_v1)
-#utils.write_dataframe_to_CSV(basic_v2, 'dataset_v2.csv')
-trans_train.get_mean_balance_amount_racio('train_sets/trans_train_modified.csv', basic_v1)
+#################################### Train Files #########################################
+#==============================================================================
+# # loading loan train file
+# loans_mod = utils.load_dataframe('train_sets/loan_train_modified.csv')
+# # create simple dataset
+# basic_v1 = create_basic_dataframe(loans_mod)
+# basic_v1.drop(basic_v1.columns[0], axis=1)
+# utils.write_dataframe_to_CSV(basic_v1, 'dataset_train_v1.csv')
+# # calculate the mean of transactions
+# basic_v2 = trans_train.get_mean_trans_values('train_sets/trans_train_modified.csv', basic_v1)
+# basic_v2 = trans_train.get_mean_balance_amount_racio('train_sets/trans_train_modified.csv', basic_v2)
+# utils.write_dataframe_to_CSV(basic_v2, 'dataset_train_v2.csv')
+#==============================================================================
 
-#print basic_v2.head()
+#################################### Test Files #########################################
+#utils.format_date_in_trans_test_CSV()
+loans_test_mod= utils.load_dataframe('test_sets/loan_test_modified.csv')
+test_v1 = create_basic_dataframe(loans_test_mod)
+test_v1.drop(test_v1.columns[0], axis=1)
+utils.write_dataframe_to_CSV(test_v1, 'dataset_test_v1.csv')
+
+test_v2 = trans_train.get_mean_trans_values('test_sets/trans_test_modified.csv', test_v1)
+test_v2 = trans_train.get_mean_balance_amount_racio('test_sets/trans_test_modified.csv', test_v2)
+utils.write_dataframe_to_CSV(test_v2, 'dataset_test_v2.csv')
 
 #########################################################################################
 
--- source/trans_train.py
+++ source/trans_train.py
@@ -9,47 +9,71 @@ import utils
 
 def get_mean_trans_values(file, dataframe):
     trans_mod = utils.load_dataframe(file)
+    
+    trans_mod.sort_values(by=['date'], ascending=[True])
+    
     grouped = trans_mod.groupby(['account_id'], as_index=False)
     
     credits_ = []
     withdrawals_ = []
     
     for account_id in dataframe['account_id']:
+        withdrawals_month = []
+        credits_month = []
+
+        current_date = None
+        
         withdrawal_amount = 0
         credit_amount = 0
-        withdrawal_cash_amount = 0
         
         withdrawal_count = 0
         credit_count = 0
-        withdrawal_cash_count = 0
         
         sub_group = grouped.get_group(account_id)
         
-        for index, row in sub_group.iterrows():
-            if row['type'] == 'credit':
-                credit_amount = credit_amount + row['amount']
-                credit_count = credit_count + 1
-            elif row['type'] == 'withdrawal':
-                withdrawal_amount = withdrawal_amount + row['amount']
-                withdrawal_count = withdrawal_count + 1
-            elif row['type'] == 'withdrawal in cash':
-                withdrawal_cash_amount = withdrawal_cash_amount + row['amount']
-                withdrawal_cash_count = withdrawal_cash_count + 1
-
-        # append the credit value
-        if credit_count == 0:
-            credits_.append(0)
+        for index, trans in sub_group.iterrows():
+            date = datetime.strptime(trans['date'], '%a, %b %d, \'%y')
+            if current_date is None or current_date == (date.month, date.year):
+                current_date = (date.month, date.year)
+                if trans['type'] == 'credit':
+                    credit_amount = credit_amount + trans['amount']
+                    credit_count = credit_count + 1
+                elif trans['type'] == 'withdrawal' or trans['type'] == 'withdrawal in cash':
+                    withdrawal_amount = withdrawal_amount + trans['amount']
+                    withdrawal_count = withdrawal_count + 1
+            else:
+                current_date = (date.month, date.year)
+                if(withdrawal_count != 0):
+                    withdrawals_month.append(withdrawal_amount / withdrawal_count)
+                    withdrawal_amount = 0
+                    withdrawal_count = 0
+                    
+                if(credit_count != 0):
+                    credits_month.append(credit_amount / credit_count)
+                    credit_amount = 0
+                    credit_count = 0
+                    
+                if trans['type'] == 'credit':
+                    credit_amount = credit_amount + trans['amount']
+                    credit_count = credit_count + 1
+                elif trans['type'] == 'withdrawal' or trans['type'] == 'withdrawal in cash':
+                    withdrawal_amount = withdrawal_amount + trans['amount']
+                    withdrawal_count = withdrawal_count + 1
+        
+        if(withdrawal_count != 0):
+            withdrawals_month.append(withdrawal_amount / withdrawal_count)       
+        
+        if(credit_count != 0):
+            credits_month.append(credit_amount / credit_count)
+        
+        if(len(withdrawals_month) != 0):
+            withdrawals_.append(round(sum(withdrawals_month) / len(withdrawals_month), 2))
         else:
-            credits_.append(credit_amount / credit_count)
-        
-        # append the withdrawal value
-        total_withdrawals = withdrawal_amount + withdrawal_cash_amount
-        total_withdrawals_count = withdrawal_count + withdrawal_cash_count
-        
-        if total_withdrawals_count == 0:
             withdrawals_.append(0)
+        if(len(credits_month) != 0):
+            credits_.append(round(sum(credits_month) / len(credits_month), 2))
         else:
-            withdrawals_.append(total_withdrawals / total_withdrawals_count)
+            credits_.append(0)
             
     dataframe['credits_mean'] = credits_
     dataframe['withdrawals_mean'] = withdrawals_
@@ -58,14 +82,18 @@ def get_mean_trans_values(file, dataframe):
 
 def get_mean_balance_amount_racio(file, dataframe):
     trans_mod = utils.load_dataframe(file)
+    
+    trans_mod.sort_values(by=['date'], ascending=[True])
+    
     grouped = trans_mod.groupby(['account_id'], as_index=False)
     
-    payments = []
+    payments_ = []
+    balances_ = []
     
     for index, row in dataframe.iterrows():
-        payments.append(row['amount'] / row['duration'])
+        payments_.append(row['amount'] / row['duration'])
         
-        sub_group = grouped.get_group(row['account_id'])
+        sub_group = grouped.get_group(int(row['account_id']))
         
         balance_per_month = []
         current_date = None
@@ -83,7 +111,12 @@ def get_mean_balance_amount_racio(file, dataframe):
                 balance_idx = balance_idx + 1
                 balance_per_month.append(trans['balance'])
         
-        print sub_group
-        print balance_per_month
+        #print sub_group
+        #print balance_per_month
+        
+        balances_.append(round(sum(balance_per_month) / len(balance_per_month), 2))
 
-        break
+    dataframe['average_balance_monthly'] = balances_
+    dataframe['payment_monthly'] = payments_
+
+    return dataframe
